d44f7dfb6286fa51440af009236204cb36045d00
ashapatel1997 <ashapromact18@gmail.com>
2019-02-05 17:45:59 +0530

    Added new Assignment

---
+++ C# Assignments/Assignment_1/Assignment_1/Employee.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_1
+{
+    public delegate void EmployeeDelegate(string message);
+    class Employee
+    {
+        private string Name, DepartmentName;
+        private int Id;
+       
+       public event EmployeeDelegate EmployeeEvent;
+
+       
+       
+        public Employee(int id, string name, string departName)
+        {
+            Id = id;
+            Name = name;
+            DepartmentName = departName;
+        }
+
+       
+        public int GetId()
+        {
+            if (EmployeeEvent != null)
+            {
+                EmployeeEvent.Invoke("GetId()");
+
+            }
+            return Id;
+        }
+
+        public int GetId(int id)
+        {
+            return id;
+        }
+
+
+        public string GetName()
+        {
+            if (EmployeeEvent != null)
+            {
+                EmployeeEvent.Invoke("GetName()");
+
+            }
+            return Name;
+        }
+
+        public string GetName(string name)
+        {
+            return name;
+        }
+
+        public string GetDepartmentName()
+        {
+            if (EmployeeEvent != null)
+            {
+                EmployeeEvent.Invoke("GetDepartmentName()");
+
+            }
+            return DepartmentName;
+        }
+       
+        public string GetDepartmentName(string departmentName)
+        {
+            return departmentName;
+        }
+
+    }
+}
---
+++ C# Assignments/Assignment_1/Assignment_1/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_1
+{
+    class Program
+    {
+       
+        static void Main(string[] args)
+        {
+           
+            int id;
+            string name,departName;
+            Console.WriteLine("Employee Id:");
+            id = int.Parse(Console.ReadLine());
+            Console.WriteLine("Employee Name:");
+            name = Console.ReadLine();
+            Console.WriteLine("Department Name:");
+            departName = Console.ReadLine()+"\n\n";
+
+            //object of Employee class
+            Employee employee = new Employee(id,name,departName);
+            
+            //event subscriber method
+            employee.EmployeeEvent += methodCalled;
+
+            //this will invoke after each method of Employee class is called.
+            void methodCalled(string message)
+            {
+                Console.WriteLine("{0} method called", message);
+            }
+
+            //print employee data
+            Console.WriteLine("Employee Id:" + employee.GetId()+"\n");
+            Console.WriteLine("Employee Name:" + employee.GetName()+"\n");
+            Console.WriteLine("Department Name:" + employee.GetDepartmentName());
+
+      
+            Console.WriteLine("\nEnter new Employee Id:");
+            id = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter new Employee Name:");
+            name = Console.ReadLine();
+            Console.WriteLine("Enter new Department Name:");
+            departName = Console.ReadLine() + "\n\n";
+       
+            //print updated employee data
+            Console.WriteLine("\nEmployee Id:" + employee.GetId(id) + "\n");
+            Console.WriteLine("Employee Name:" + employee.GetName(name) + "\n");
+            Console.WriteLine("Department Name:" + employee.GetDepartmentName(departName) + "\n");
+
+
+        }
+    }
+}
